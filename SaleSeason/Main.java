f862a2fa678237c8e964752ecc92169cf87f92bb
Vasanth1724 <kvasanthkumar1724@gmail.com>
2024-10-30 10:12:40 +0530

    First commit

---
+++ SaleSeason/Main.java
@@ -0,0 +1,29 @@
+package SaleSeason;
+
+import java.util.*;
+import java.lang.*;
+
+
+class Main
+{
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int x=sc.nextInt();
+
+            if(x<=100){
+                System.out.println(x);
+            }else if(x>100 && x<=1000){
+                System.out.println(x-25);
+            }else if(x>1000 && x<=5000){
+                System.out.println(x-100);
+            }else {
+                System.out.println(x-500);
+            }
+
+        }
+
+    }
+}
