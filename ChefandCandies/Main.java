661d4a63337e15b8e8b90f719453fcefbbea4ca8
Vasanth1724 <kvasanthkumar1724@gmail.com>
2024-11-02 20:21:57 +0530

    First commit

---
+++ ChefandCandies/Main.java
@@ -0,0 +1,23 @@
+package ChefandCandies;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+
+            int candies=n-x;
+
+            int packets=(candies>0) ? (candies+3)/4 : 0;
+
+            System.out.println(packets);
+        }
+
+    }
+}
